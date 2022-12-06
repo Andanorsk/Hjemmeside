ec96be623fce1bddb21653dbb36e768ca6e24806
Odd Erik Haaskjold <oebhaa@gmail.com>
2022-12-06 19:59:40 +0100

    flask før db

--- README.md
+++ README.md
@@ -5,11 +5,21 @@ https://stackoverflow.com/questions/69605313/vs-code-terminal-activate-ps1-canno
 
 Hvordan sette opp flask
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+https://flask.palletsprojects.com/en/2.1.x/tutorial/
+
+
 
 Hvordan sette opp venv i VS code
 1. Install extension: Python fra mixcrosoft.com
 2. Install python 3.10
-3. sett opp venv ved å velgge: 
+3. sett opp venv ved å velge: ?
 
 Husk å sette miljø(windows) variabel: 
 $ set FLASK_APP=main.py
+
+
+For å lagre de installeretye pakkene i en fil:
+$ pip freeze > requirements.txt 
+
+For å installere dette på en annen maskin: 
+$ pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'wergfc3453f63d4uwger6uf5trdf42d5'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/amalie')
+def amalie():
+    return render_template('amalie_skram.html')
+
+@app.route('/prod')
+def prod():
+    return render_template('prod.html')
+
+@app.route('/ikt')
+def ikt():
+    return render_template('IKT.html')
+
+@app.route('/artikkel')
+def artikkel():
+    # hente data
+    data=[]
+    data.append({"tittel":"Forms", "url":"https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application", "beskrivelse":"Her fant jeg hvordan jeg skulle bruke forms i flask"})
+    data.append({"tittel":"Flask tutorial", "url":"https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world", "beskrivelse":"Her lærte jeg Flask"})
+    return render_template('artikkel.html', data=data)
+
+@app.route('/input', methods=('GET', 'POST') )
+def input():
+    if request.method == 'POST':
+        tittel = request.form['tittel']
+        url = request.form['url']
+        beskrivelse = request.form['beskrivelse']
+        # Lagre dataene
+
+        # Returnere OK hvis lagret
+        return  render_template('input.html', okornot="Dataene ble lagret")
+    if request.method == 'GET':
+        return render_template('input.html', okornot="")
+
+if __name__ == '__main__':
+    # APP.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
