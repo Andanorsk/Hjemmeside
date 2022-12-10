ac06027afe3a29d88b5be788d7df49faab839b6f
Odd Erik Haaskjold <oebhaa@gmail.com>
2022-12-10 16:09:17 +0100

    ojk

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# Flask er en python pakke som fungerer som en webserver.
 from flask import Flask, render_template, request, url_for, flash, redirect
 
+# Her starter flask appen og den konffigureres   
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'wergfc3453f63d4uwger6uf5trdf42d5'
 
+# Fuksjonene som er under er de ulike adressene for websidene
+# app.route er en dekrotør(dekorator)som setter hvilke addresser denne skal  låse på
 @app.route('/')
 def index():
+    # render_template er en innebygget flask funksjon som returnerer en mal html side til brukeren. 
+    # Man kan sende data med dette svaret som kan brukes av siden. Se foreskempel artikkelfunksjonen.
     return render_template('index.html')
 
 @app.route('/amalie')
