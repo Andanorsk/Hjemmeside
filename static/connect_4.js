3c45ceb66943c616af31799cce302049ebf41fc2
Fortnite God <oebhaa@gmail.com>
2023-02-19 17:47:33 +0100

    ferdig med spill basics

--- static/connect_4.js
+++ static/connect_4.js
@@ -78,13 +78,64 @@ function checkWinner() {
       }
     }
   }
+
+  //vertical
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 3; r++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c] &&
+          board[r + 1][c] == board[r + 2][c] &&
+          board[r + 2][c] == board[r + 3][c]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  // anti diagonally
+  for (let r = 0; r < rows - 3; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c + 1] &&
+          board[r + 1][c + 1] == board[r + 2][c + 2] &&
+          board[r + 2][c + 2] == board[r + 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  //diagonal
+  for (let r = 3; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r - 1][c + 1] &&
+          board[r - 1][c + 1] == board[r - 2][c + 2] &&
+          board[r - 2][c + 2] == board[r - 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  //avslutter check winner
 }
-function setWinner (r,c) {
-  let winner = document.getElementById("Winner")
-  if(board[r][c] = playerRed){
-    winner.innerText = "Red Wins"
+
+function setWinner(r, c) {
+  let winner = document.getElementById("Winner");
+  if (board[r][c] == playerRed) {
+    winner.innerText = "Red Wins";
   } else {
-    winner.innerText = "Yellow Wins"
+    winner.innerText = "Yellow Wins";
   }
   gameOver = true;
 }
